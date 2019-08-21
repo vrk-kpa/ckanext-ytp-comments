38107d9b3a3bea035d80cb8344ab3eb199e3873f
Nathan Perry <nathan@salsadigital.com.au>
2019-08-21 16:38:12 +1200

    - Add the necessary changes from outstanding PR: https://github.com/vrk-kpa/ckanext-ytp-comments/pull/1 - Add `lxml` to requirements.txt - Add a new javascript file (`comment_validation.js`) to implement front-end JS validation of comment forms - Add error message elements to the `comment_list.html` template

---
+++ ckanext/ytp/comments/public/javascript/comment_validation.js
@@ -0,0 +1,28 @@
+function hideFormErrors()
+{
+    jQuery('.form_errors').addClass('hidden');
+    jQuery('.form_errors li').addClass('hidden');
+}
+
+jQuery(document).ready(function() {
+
+    jQuery('.module-content input[type="submit"]').on('click', function(e) {
+        if (jQuery(this).hasClass('btn-primary')) {
+            var form = jQuery(this).closest('form');
+            var comment = form.find('textarea[name="comment"]').val();
+            var display_errors = false;
+
+            hideFormErrors();
+
+            if (!comment) {
+                form.find('.error-comment').removeClass('hidden');
+                display_errors = true;
+            }
+            if (display_errors) {
+                form.find('.form-errors').removeClass('hidden');
+                return false;
+            }
+        }
+    });
+
+});
